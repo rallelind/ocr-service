ab7570da7eb0e2389367f3fb30cd7084745f375b
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-17 22:11:19 +0200

    comment

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use aws_sdk_textract as textract;
 
+// Resources for learning: 
+// https://medium.com/intelliconnect-engineering/uploading-files-to-aws-s3-using-axum-a-rust-framework-c96b1c774dfc
+// https://www.youtube.com/watch?v=DLmyW58egg4
+
 #[tokio::main]
 async fn main() -> Result<(), textract::Error> {
     let config = aws_config::load_from_env().await;
