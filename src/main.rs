5c5b34f214006d9b2bc3918cd85d1cb16a160276
Rasmus Lind <rasmus@voiceboxer.com>
2023-04-23 13:19:11 +0200

    receive message from queue

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use aws_sdk_textract as textract;
+use aws_sdk_sqs::{Client, Error};
+use dotenv::dotenv;
+
+async fn receive(client: &Client, queue_url: &String) -> Result<(), Error> {
+    let rcv_message_output = client.receive_message().queue_url(queue_url).send().await?;
+
+    println!("Messages from queue with url: {}", queue_url);
+
+    for message in rcv_message_output.messages.unwrap_or_default() {
+        println!("Got the message: {:#?}", message);
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
-async fn main() -> Result<(), textract::Error> {
-    let config = aws_config::load_from_env().await;
-    let client = textract::Client::new(&config);
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
 
-    // ... make some calls with the client
-    
+    let queue_url = std::env::var("AWS_SQS_URL").expect("aws sqs url should be provided");
+
+    let config = aws_config::load_from_env().await;
+    let textract_client = textract::Client::new(&config);
+    let sqs_client = Client::new(&config);
+
+    receive(&sqs_client, &queue_url).await?;
+
 
     Ok(())
 }
